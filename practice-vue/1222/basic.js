93af2449b83d63594fa7b1962355e1509c94d3b9
Junha <prodopark15@gmail.com>
2021-12-27 18:58:54 +0900

    update chapter 1,2,3

---
+++ practice-vue/1222/basic.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue.js',
+    name: 'junha',
+    uid: '10',
+    flag: false,
+  },
+  methods: {
+    clickBtn() {
+      console.log('hi');
+    },
+  },
+});
